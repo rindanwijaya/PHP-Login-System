406cedbb0d8c85533b666822a691f536f1e509ee
Rino Ramadan <rindan.wijaya@gmail.com>
2019-03-20 10:34:14 +0700

    add form login

--- index.php
+++ index.php
@@ -1,5 +1,54 @@
-<?php
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="robots" content="follow">
+    <title>Rino RIM</title>
+    <base href="/"/>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
+  </head>
+  <body>
+        <div class="uk-section uk-container uk-text-left">
+            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+              <form class="uk-form-stacked js-login">
 
-echo "Rino Keren";
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Email</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" required="required" type="email" placeholder="email@email.com">
+                    </div>
+                </div>
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Password</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" required="required" type="password" placeholder="Your Password">
+                    </div>
+                </div>
 
-?>
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-deffault" type="submit">Login</button>
+                </div>
+
+
+              </form>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+        <!-- UIkit JQUERY -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- UIkit JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+  </body>
+</html>
